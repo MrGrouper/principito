fc2d2ef977c61d722e3d06b325af99fcaab6912f
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2024-01-22 12:29:29 -0600

    update key, and config

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,8 @@ function App() {
   const [text, setUsername] = useState('hello world')
   const [language, setLanguage] = useState('es')
   const input = useRef(null)
-  const apiKey = import.meta.env.VITE_REACT_APP_API_KEY
-  // const apiKey = 'AIzaSyD4hE53CpUcoqbrvKIqR7unBm84HaUrbGo'
 
-  console.log(apiKey)
 
-  const handleClick=(e)=>{
-    console.log(e.target.innerText)
-}
-
-  const test = () => {
-    console.log(input.current)
-
-  }
 
   return (
     
