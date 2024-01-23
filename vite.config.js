fee76c5cf97a7df8eccda4b59c4a71c80f6c8178
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2024-01-23 15:22:51 -0600

    Update vite config and deloy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()], 
-  base: "/principito/"
+  base: "/principito/",
+  define: {
+    'import.meta.env.VITE_REACT_APP_API_KEY': JSON.stringify(process.env.VITE_REACT_APP_API_KEY),
+  },
 })
